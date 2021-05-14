f97eafcc00dd3453e9f1be81cd94aeb23b7d4448
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-05-14 11:32:05 +0500

    getting ready for production

--- bilim/settings.py
+++ bilim/settings.py
@@ -157,6 +157,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         # 'rest_framework.permissions.IsAuthenticated',
     ),
-#     'DEFAULT_PAGINATION_CLASS': 'bilim.pagination.CustomPagination',
-#     'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'bilim.pagination.CustomPagination',
+    'PAGE_SIZE': 10,
 }

1ecf4de31ebc962c3cb6ee4c78ea29031ae2594c
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-06-03 14:58:49 +0500

    h

--- main/views.py
+++ main/views.py
@@ -96,6 +96,7 @@ class LoginView(APIView):
 
 
 class MeView(APIView):
+    permission_classes = (Or(IsAdminUser, IsAuthenticated),)
 
     @staticmethod
     def get(request):
@@ -169,7 +170,7 @@ class UserDetailView(generics.RetrieveAPIView):
 
 class UserUpdateView(generics.GenericAPIView):
     serializer_class = UserSerializer
-
+    permission_classes = (Or(IsAdminUser, IsAuthenticated),)
     @staticmethod
     def patch(request):
         try:
@@ -243,6 +244,7 @@ class UserDeleteView(generics.GenericAPIView):
 
 class UserQuestionsView(generics.ListAPIView):
     serializer_class = QuestionSerializer
+    permission_classes = (Or(IsAdminUser, IsAuthenticated),)
 
     def get_queryset(self):
         try:
@@ -349,6 +351,7 @@ class QuestionDeleteView(generics.DestroyAPIView):
 
 class AnswerListView(generics.ListAPIView):
     serializer_class = AnswerSerializer
+    permission_classes = (Or(IsAdminUser, IsAuthenticated),)
 
     def get_queryset(self):
         queryset = Answer.objects.all()

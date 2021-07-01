b36d67ea429a652f85bae0246aa4634d506d0f15
MirjahonMirsaidov <mirjahonac021@gmail.com>
2021-07-01 17:29:00 +0500

    setup

--- bilim/settings.py
+++ bilim/settings.py
@@ -107,8 +107,11 @@ WSGI_APPLICATION = 'bilim.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
     }
 }
 
